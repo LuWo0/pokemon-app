aa41029880c9a4f5c33fe926f5c326d9e29e3517
LuWo0 <loviedo46@yahoo.com>
2022-10-09 02:11:50 -0700

    finished basic adding of pokemon

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,50 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios';
+import PokemonList from './PokemonList';
 
 function App() {
   const [pokemon, setPokemon] = useState([]);
+  const [currentPoke, setCurrentPoke] = useState(1);
+  const [loading, setLoading] = useState(true);
+  
 
   useEffect(() => {
-    const getPokemonList = async () => {
+    const getPokemon = async () => {
       try{
-        const jsonBlob = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=649&offset=0");
-        const pokeList = await jsonBlob.data.results;
-        setPokemon(pokeList);
+        const jsonBlob = await axios.get(`https://pokeapi.co/api/v2/pokemon/${currentPoke}`);
+        const pokeData = await jsonBlob.data;
+        setLoading(true);
+        return pokeData;
       } catch(err){
         console.error(err);
+        return;
       }
     }
-    getPokemonList();
-  }, []);
 
+    getPokemon()
+      .then(data => {
+        setLoading(false);
+        setPokemon(pokemon => [...pokemon, data]);
+      })
+      .catch(err => {
+        console.log(err, "Error in the getPokemon");
+      });
+    
+  }, [currentPoke]); // rerender whenever we click 
+
+
+  const getNextPokemon = (e) => {
+    e.preventDefault();
+    setCurrentPoke(currentPoke + 1);
+  }
+
+  if (loading) return "Loading...";
+  console.log(pokemon);
   return (
-    <div>
-      {pokemon.map(poke => (<p>{poke.name}</p>))}
-    </div>
+    <>
+      <PokemonList pokemon={pokemon} />
+      <button onClick={getNextPokemon}>Next</button>
+    </>
   )
 }
 
